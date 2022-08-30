5c3a4af7c3e29a7da6b021adcdfc13ce31920b37
Samuel <vup815@gmail.com>
2022-08-30 16:48:45 +0800

    20220830

--- homebrew.sh
+++ homebrew.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Run the Homebrew Script
-brew bundle --file ./Brewfile
+
 
 
